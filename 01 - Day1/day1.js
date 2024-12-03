df5605ada406372c41245d14b01843c6ba25350b
bouchta65 <bouchtamohamed01@gmail.com>
2024-12-03 09:21:45 +0100

    Switch from html to js

--- 01 - Day1/day1.js
+++ 01 - Day1/day1.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
+
 const data = [
   [56208, 95668],
   [52621, 74203],
@@ -1050,6 +1042,3 @@ function SumDifferences2() {
     return result2;
 }
 console.log(SumDifferences2())
-    </script>
-</body>
-</html>
